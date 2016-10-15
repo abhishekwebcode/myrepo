521b07d36a3ee1342e7f68d0032d3a2554c8ac8e
AbhishekMathur <mathur17021play@gmail.com>
2016-10-15 20:11:30 +0530

    commit

--- app/src/main/java/com/abhishekmathur/coolcalculator_tutorial/MainActivity.java
+++ app/src/main/java/com/abhishekmathur/coolcalculator_tutorial/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //comment
         Toast.makeText(this, "hello world", Toast.LENGTH_SHORT).show();
     }
 }
